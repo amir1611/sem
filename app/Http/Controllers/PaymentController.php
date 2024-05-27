dd38c6f864286a1e0ba0cb38b6f47bbba421c259
apikammar <apikammar07@gmail.com>
2024-05-27 11:21:15 +0800

    update

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -62,9 +62,6 @@ class PaymentController extends Controller
             // Save the payment
             $payment->save();
 
-            // You may add a success message or redirect to a success page
-            return view('managePayment.kioskParticipant.pendingApproval');
-
             } catch (\Throwable $th) {
            // Display error message along with the confirmation message
             return view('error', [
